b2db27c28b68555f39cea2d2c4b88de812adede1
penscola <penscolashackletonfelix@gmail.com>
2023-03-05 17:32:21 +0300

    Hooking up sanity

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-import { Product, FooterBanner, HeroBanner } from '@/components'
+import { client } from '@/lib/client';
+import { Product, FooterBanner, HeroBanner } from '@/components';
 
-function Home() {
+function Home({ products, bannerData }) {
   return (
     <div>
-        <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
 
-        <div className="products-heading">
-          <h2>Best Selling Products</h2>
-          <p>Speakers of many variations</p>
-        </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
 
-        <div className="products-container">
-          {['product 1', 'Product 2'].map(
-            (product) => product)}
-        </div>
-
-        <FooterBanner />
+      <FooterBanner />
     </div>
-  )
+  );
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
